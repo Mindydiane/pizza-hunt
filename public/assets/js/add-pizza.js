c5692aaef9e027690867c38c6bb60203b52b8d45
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-09-07 18:01:15 -0500

    lastest change

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -49,23 +49,26 @@ const handlePizzaSubmit = event => {
   if (!pizzaName || !createdBy || !toppings.length) {
     return;
   }
+
+  const formData = { pizzaName, createdBy, size, toppings };
+
   fetch('/api/pizzas', {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     },
-    // body: JSON.stringify(formData)
+    body: JSON.stringify(formData)
   })
     .then(response => response.json())
     .then(postResponse => {
-      alert('Pizza created successfully!');
       console.log(postResponse);
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);
+      // DO INDEXED DB STUFF HERE
     });
-  const formData = { pizzaName, createdBy, size, toppings };
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
